fcd5481f942e47fd30145fecc2768b1b206ad482
phellipeh <phellipeh@bol.com.br>
2014-08-02 00:49:19 -0300

    Ajustes no ADS-B Decoder

--- Servidor/adsbDecoder.py
+++ Servidor/adsbDecoder.py
@@ -9,78 +9,41 @@ def toHex(x):
 def toBin(x):
     return bin(eval("0b" + x))
 
+def full_bit_zero(data):
+    data = data[2:]
+    falta = 8 - len(data)
+    for x in range(0, falta):
+        data= '0' + data
+    return data
+
+def c(d1, d2):
+    return full_bit_zero(bin(eval(toHex(d1+d2))))
+
 def ADSBDataDecoder(data):
-    #Primeiro Byte para DF+CA
-    DFCA = toHex(data[0]+data[1])
-
-    #Dois bytes para ICAO
-    ICAO = "0x"+data[2]+data[3]+" 0x"+data[4]+data[5]+" 0x"+data[6]+data[7]
-
-    #Cinco Bytes para ADSB Data
-    ADSB_Data = "0x"+data[8]+data[9]+" 0x"+data[10]+data[11]+" 0x"+data[12]+data[13]+" 0x"+data[14]+data[15]+" 0x"+data[16]+data[17]+" 0x"+data[18]+data[19]+" 0x"+data[20]+data[21]
-
-    b_DFCA = bin(eval(DFCA))
-    DF = b_DFCA[2]+b_DFCA[3]+b_DFCA[4]+b_DFCA[5]+b_DFCA[6]
-    CA = b_DFCA[7]+b_DFCA[8]+b_DFCA[9]
-
-    TC = toHex(data[8]+data[9])
-    b_TC = bin(eval(TC))
-    b_TC = b_TC[2]+b_TC[3]+b_TC[4]+b_TC[5]+b_TC[6]
-
-    Alt_TF_hex_1 = toHex(data[10]+data[11])
-    Alt_TF_hex_2 = toHex(data[12]+data[13])
-
-    qb_Alt_TF_hex_2 = bin(eval(Alt_TF_hex_2))
+    DFCA = toHex(data[:2])                                                      #Primeiro Byte para DF+CA
+    ICAO = "0x"+data[2]+data[3]+" 0x"+data[4]+data[5]+" 0x"+data[6]+data[7]     #Tres bytes para ICAO
+    b_TC = full_bit_zero(bin(eval(toHex(data[8]+data[9]))))[:5]                 #Um Byte para o Type Code
     
-    T = qb_Alt_TF_hex_2[6]
-    F = qb_Alt_TF_hex_2[7]
+    if b_TC == "01011":
+        print "Airborne Position message... Obtendo Dados (Altitude, Latitude e Longitude)"
+        hex_adsb_packet = data[10:]
+        bin_adsb_packet = c(hex_adsb_packet[0],hex_adsb_packet[1])+c(hex_adsb_packet[2],hex_adsb_packet[3])+c(hex_adsb_packet[4],hex_adsb_packet[5])+c(hex_adsb_packet[6],hex_adsb_packet[7])+c(hex_adsb_packet[8],hex_adsb_packet[9])+c(hex_adsb_packet[10],hex_adsb_packet[11])
+
+        Altitude = eval("0b" + bin_adsb_packet[:12])
+        '''Latitude = eval("0b" + Latitude)
+        Longitude =  eval("0b" + Longitude)
+'''
+        print "Altitude: " + str(Altitude)
+        print "(Dec) Latitude:  " + str(Latitude)
+        print "(Dec) Longitude: " + str(Longitude)
+
     
-    db_restantes = qb_Alt_TF_hex_2[8]+qb_Alt_TF_hex_2[9]
+
+
+        
+    elif b_TC == "10011":
+        print "Airborne Velocity message"
+
     
-    qb_Alt_TF_hex_2 = qb_Alt_TF_hex_2[2]+qb_Alt_TF_hex_2[3]+qb_Alt_TF_hex_2[4]+qb_Alt_TF_hex_2[5]
-    
-    b_Alt = bin(eval(Alt_TF_hex_1))
-    b_Alt = b_Alt[2]+b_Alt[3]+b_Alt[4]+b_Alt[5]+b_Alt[6]+b_Alt[7]+qb_Alt_TF_hex_2
-
-    aux = toHex(data[14]+data[15])
-    aux = bin(eval(aux))
-    
-    aux2 = toHex(data[16]+data[17])
-    aux2 = bin(eval(aux2))
-    
-    Latitude = db_restantes+aux[2]+aux[3]+aux[4]+aux[5]+aux[6]+aux[7] + aux2[2]+aux2[3]+aux2[4]+aux2[5]+aux2[6]+aux2[7]+aux2[8]
-
-    aux3 = toHex(data[18]+data[19])
-    aux3 = bin(eval(aux3))
-
-    aux4 = toHex(data[20]+data[21])
-    aux4 = bin(eval(aux4))
-    
-    Longitude = aux2[9]+aux3[2]+aux3[3]+aux3[4]+aux3[5]+aux3[6]+aux3[7]+aux3[8]+aux3[9] + aux4[2]+aux4[3]+aux4[4]+aux4[5]+aux4[6]+aux4[7]+aux4[8]+aux4[9]  
-
-    print "(Raw) DF+CA: " + DFCA
-    print "(Raw) ICAO: " + ICAO
-    print "(Raw) ADSB-Data: " + ADSB_Data
-    print ""
-    print "(Bin) DF+CA: " + b_DFCA
-    print "(Bin) DF: " + DF
-    print "(Bin) CA: " + CA
-    print "(Bin) Type Code: " + b_TC
-    print "(Bin) Altitude: " + b_Alt
-    print "(Bin) T: " + T
-    print "(Bin) F: " + F 
-    print ""
-    print "(Bin) Latitude:  " + Latitude
-    print "(Bin) Longitude: " + Longitude
-    print ""
-
-    b_Alt = eval("0b" + b_Alt)
-    Latitude = eval("0b" + Latitude)
-    Longitude =  eval("0b" + Longitude)
-
-    print "(Dec) Altitude: " + str(b_Alt)
-    print "(Dec) Latitude:  " + str(Latitude)
-    print "(Dec) Longitude: " + str(Longitude)
-
     
 ADSBDataDecoder("8d4008f15837f237ebe3a3")
